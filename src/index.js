17ad6e3a7f82e29906294efa2af9b6963190cc7d
System Administrator <root@Brendans-MacBook-Air.local>
2022-04-15 11:55:37 -0400

    deploy

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const { ApolloServer } = require('apollo-server');
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+const port = process.env.PORT || 8080;
+
+const server = new ApolloServer({
+    resolvers,
+    typeDefs
+});
+
+server.listen({ port}, () => console.log("Server started on Port 8080"));
